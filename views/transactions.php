e3a7aec259a0a48b9c45371f2674c6cf1fbe35dc
bima-taruna <bimataruna21@gmail.com>
2025-02-27 21:47:10 +0700

    Add styling for income and expense classification in transactions table

--- views/transactions.php
+++ views/transactions.php
@@ -1,55 +1,74 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Transactions</title>
-        <style>
-            table {
-                width: 100%;
-                border-collapse: collapse;
-                text-align: center;
-            }
 
-            table tr th, table tr td {
-                padding: 5px;
-                border: 1px #eee solid;
-            }
+<head>
+    <title>Transactions</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            text-align: center;
+        }
 
-            tfoot tr th, tfoot tr td {
-                font-size: 20px;
-            }
+        table tr th,
+        table tr td {
+            padding: 5px;
+            border: 1px #eee solid;
+        }
 
-            tfoot tr th {
-                text-align: right;
-            }
-        </style>
-    </head>
-    <body>
-        <table>
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Check #</th>
-                    <th>Description</th>
-                    <th>Amount</th>
-                </tr>
-            </thead>
-            <tbody>
-                <!-- YOUR CODE -->
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th colspan="3">Total Income:</th>
-                    <td><!-- YOUR CODE --></td>
-                </tr>
-                <tr>
-                    <th colspan="3">Total Expense:</th>
-                    <td><!-- YOUR CODE --></td>
-                </tr>
-                <tr>
-                    <th colspan="3">Net Total:</th>
-                    <td><!-- YOUR CODE --></td>
-                </tr>
-            </tfoot>
-        </table>
-    </body>
+        tfoot tr th,
+        tfoot tr td {
+            font-size: 20px;
+        }
+
+        tfoot tr th {
+            text-align: right;
+        }
+
+        .expense {
+            color: red;
+        }
+
+        .income {
+            color: green;
+        }
+    </style>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Check #</th>
+                <th>Description</th>
+                <th>Amount</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            require_once APP_PATH . "App.php";
+            $result = getCsvContent(FILES_PATH . "sample_1.csv");
+            $income = [];
+            $expense = [];
+            processTheArray($result, $expense, $income);
+            ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="3">Total Income:</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th colspan="3">Total Expense:</th>
+                <td><!-- YOUR CODE --></td>
+            </tr>
+            <tr>
+                <th colspan="3">Net Total:</th>
+                <td><!-- YOUR CODE --></td>
+            </tr>
+        </tfoot>
+    </table>
+</body>
+
 </html>

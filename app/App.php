ede7ec317d00a11718d7d116ea4e909ffad1593c
bima-taruna <bimataruna21@gmail.com>
2025-02-27 20:22:46 +0700

    Add CSV content retrieval function with error handling

--- app/App.php
+++ app/App.php
@@ -1,5 +1,20 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 // Your Code
+
+function getCsvContent(string $filepath): array
+{
+    $result = [];
+    if (!file_exists($filepath)) {
+        echo "file not found!";
+        return [];
+    }
+
+    $file = fopen($filepath, "r");
+    while (($line = fgetcsv($file)) !== false) {
+        array_push($result, $line);
+    }
+    return $result;
+}
